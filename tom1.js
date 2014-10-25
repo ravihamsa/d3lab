f4f98d831ab3c478f559c88dc7787282e336d1ac
Ravi Kumar <ravi.hamsa@C1MN8AXRDTY3.local>
2014-10-25 16:48:41 +0530

    drag drop and day scale complete

--- tom1.js
+++ tom1.js
@@ -4,12 +4,16 @@
     var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 
+    var msPerDay = 1000 * 60 * 60 * 24;
+
     var LBPController = function (config) {
         this.rootEl = config.rootEl;
         this.fullTimelineDays = config.fullTimelineDays || 1000;
         this.dayWidth = config.dayWidth || 50;
+        this.dayHeight = config.dayHeight || 70;
+        this.mileStonesPerColumn = config.mileStonesPerColumn || 3;
         this.rootWidth = parseInt(this.rootEl.style('width'));
-        this.rootHeight = config.rootHeight || 400;
+        this.rootHeight = config.rootHeight || 500;
 
         this.visibleTimelineDays = Math.ceil(this.rootWidth / this.dayWidth);
         this.tableMargin = {
@@ -24,8 +28,9 @@
 
         this.startYear = config.startYear || 2013;
         this.startMonth = config.startMonth || 0;
-        this.startDay = config.startDay || 1;
+        this.startDay = config.startDay || 0;
         this.scale = config.scale || 'day';
+        this.scaleOffset = 0;
 
         this.rootEl.select('.lbp-inner')
             .style('left', this.tableMargin.left + 'px')
@@ -33,6 +38,7 @@
             .style('width', this.rootInnerWidth + 'px')
         //.style('height', this.rootInnerHeight +'px')
     }
+
 
     LBPController.prototype = {
         initialize: function () {
@@ -43,6 +49,7 @@
         renderLBP: function () {
             this.renderTimeLine(this.rootEl.select('.time-line'))
             this.renderGrid(this.rootEl.select('.grid-container'))
+            this.renderWorkStream();
         },
         setWorkStreamData: function (workStreamData) {
             this.workStreamData = workStreamData;
@@ -128,8 +135,12 @@
                     return monthArray[d.month];
                 })
 
-            days.text(function (d) {return d.date})
-                .attr('class', function (d) { return 'day d' + d.day })
+            days.text(function (d) {
+                return d.date
+            })
+                .attr('class', function (d) {
+                    return 'day d' + d.day
+                })
         },
         updateTimeLine: function () {
             this.refreshTimeLineData();
@@ -153,6 +164,8 @@
             this._timeLineEnter();
             this._timeLineExit();
             this._timeLineUpdate();
+
+            this.updateWorkStream();
         },
         renderTimeLine: function (root) {
 
@@ -216,9 +229,9 @@
 
 
         },
-        generateTimeLineData: function () {
-            var steps = 1;
+        getDayTimeLineData: function(){
             var data = [];
+            var steps = 1;
             for (var i = 0, counter = 0; counter < this.visibleTimelineDays; i += steps) {
                 var date = new Date(this.startYear, this.startMonth, this.startDay + i);
                 var day = date.getDay();
@@ -235,24 +248,48 @@
             }
             return data;
         },
-        getWorkStreamData: function (workStreamCount) {
+        getMonthTimeLineData: function(){
             var data = [];
-            for (var i = 0; i < workStreamCount; i++) {
+            var steps = 1;
+            for (var i = 0, counter = 0; counter < this.visibleTimelineDays; i += steps) {
+                var date = new Date(this.startYear, this.startMonth+i, this.startDay );
+                var day = date.getDay();
+                //if (day !== 0 && day !== 6) {
                 data.push({
-                    name: 'workStream_' + i,
-                    id: i,
-                    milestones: []
+                    id: date.getDate() + '_' + date.getMonth() + '_' + date.getFullYear(),
+                    day: date.getDay(),
+                    date: date.getDate(),
+                    month: date.getMonth(),
+                    year: date.getFullYear()
                 })
+                counter++;
+                // }
             }
+            console.log(data);
             return data;
+        },
+        generateTimeLineData: function () {
+
+
+            switch(this.scale){
+                case 'day':
+                    return this.getDayTimeLineData();
+                    break;
+                case 'month':
+                    return this.getMonthTimeLineData();
+                    break;
+
+            }
+
         },
         renderGrid: function (root) {
             var _this = this;
             var gridExtension = 30;
-            var workStreamData = this.getWorkStreamData(4);
+            var workStreamData = this.getWorkStreamData();
             var workStreamCount = workStreamData.length;
             var rowCount = workStreamCount * 3;
             var dayWidth = this.dayWidth;
+            var dayHeight = this.dayHeight;
             var rootInnerWidth = this.rootInnerWidth;
 
             var visibleTimeLineDays = Math.ceil(rootInnerWidth / dayWidth);
@@ -266,12 +303,14 @@
 
             var svg = root.append('svg')
                 .attr('width', rootInnerWidth + gridExtension)
-                .attr('height', rowCount * dayWidth)
+                .attr('height', rowCount * dayHeight)
+
+            this.gridSVG = svg;
 
 
             var gridRect = svg.append('rect')
                 .attr('width', rootInnerWidth)
-                .attr('height', rowCount * dayWidth)
+                .attr('height', rowCount * dayHeight)
                 .attr('stroke', '#eee')
                 .attr('fill', 'transparent')
                 .attr('x', gridExtension)
@@ -295,7 +334,7 @@
                 .append('g')
                 .attr('class', 'row')
                 .attr('transform', function (data, index) {
-                    var y = index * 3 * dayWidth;
+                    var y = index * 3 * dayHeight;
                     return 'translate(0, ' + y + ')';
                 })
 
@@ -330,7 +369,7 @@
                 .attr('class', 'col')
                 .attr('d', function (d) {
                     var x = d * dayWidth;
-                    return 'M ' + x + ' 0 L ' + x + ' ' + (rowCount * dayWidth);
+                    return 'M ' + x + ' 0 L ' + x + ' ' + (rowCount * dayHeight);
                 })
                 .attr('stroke', '#ddd')
                 .attr('stroke-width', strokeWidth)
@@ -348,7 +387,7 @@
                 _this.updateTimeLine();
             }
 
-            var dragStart = function(d){
+            var dragStart = function (d) {
                 d.x = _this.startDay * _this.dayWidth;
             }
 
@@ -361,7 +400,209 @@
 
             gridRect.call(drag);
 
+        },
+        getMileStoneData: function (workSteamData) {
+            var mileStones = [];
+            _.each(workSteamData.milestones, function (milestone) {
+                mileStones.push(milestone);
+            })
+
+            var mileStoneData = _.map(_.groupBy(mileStones, function (item) {
+                return item.date;
+            }), function (item, index) {
+                return {
+                    date: index,
+                    milestones: item
+                }
+            });
+            return mileStoneData;
+        },
+        redrawWorkStream: function(root){
+            var _this = this;
+            var workStreamData = _this.getWorkStreamData();
+            var startDate = new Date(this.startYear, this.startMonth, this.startDay).getTime();
+            var mileStonesPerColumn = _this.mileStonesPerColumn;
+
+
+
+            var workStreams = root.selectAll('g.ws')
+                .data(workStreamData)
+                .attr('transform', function (d, index) {
+                    var y = index * (_this.dayHeight * _this.mileStonesPerColumn);
+                    y += 25;
+                    return 'translate(0, ' + y + ')'
+                })
+
+
+            var dates = workStreams.selectAll('g.dt')
+                .data(function (d) {
+                    return _this.getMileStoneData(d)
+                })
+                .attr('transform', function (d) {
+                    var date = d.date;
+                    var dayDiff = (date - startDate) / msPerDay;
+                    var x = dayDiff * _this.dayWidth;
+                    var y = 0;
+                    return 'translate(' + x + ',' + y + ')';
+                })
+
+
+            var mileStones = dates.selectAll('g.ms')
+                .data(function (d) {
+                    console.log(d);
+                    return d.milestones
+                })
+                .attr('transform', function (d, index) {
+                    console.log(d.name,  new Date(d.date));
+                    var x = Math.floor(index / mileStonesPerColumn) * 5;
+                    var y = (index % mileStonesPerColumn) * _this.dayHeight;
+                    d.xdif = x;
+                    d.ydif = y;
+                    d.x = 0;
+                    d.y = 0;
+                    return 'translate(' + x + ',' + y + ')';
+                })
+
+        },
+        renderWorkStream: function () {
+            var _this = this;
+            var workStreamData = _this.getWorkStreamData();
+            var startDate = new Date(this.startYear, this.startMonth, this.startDay).getTime();
+            var mileStonesPerColumn = _this.mileStonesPerColumn;
+
+            var colorFun = d3.scale.category10()
+            this.gridSVG.select('g.work-streams').remove();
+            var root = this.gridSVG.append('g').attr('class', 'work-streams');
+            var workStreams = root.selectAll('g.ws')
+                .data(workStreamData)
+                .enter()
+                .append("g")
+                .attr('class', 'ws')
+                .attr('transform', function (d, index) {
+                    var y = index * (_this.dayHeight * _this.mileStonesPerColumn);
+                    y += 25;
+                    return 'translate(0, ' + y + ')'
+                })
+
+
+            var dates = workStreams.selectAll('g.dt')
+                .data(function (d) {
+                    return _this.getMileStoneData(d)
+                })
+                .enter()
+                .append("g")
+                .attr('class', 'dt')
+                .attr('transform', function (d) {
+                    var date = d.date;
+                    var dayDiff = (date - startDate) / msPerDay;
+                    var x = dayDiff * _this.dayWidth;
+                    var y = 0;
+                    return 'translate(' + x + ',' + y + ')';
+                })
+
+
+            var mileStones = dates.selectAll('g.ms')
+                .data(function (d) {
+                    return d.milestones
+                })
+                .enter()
+                .append('g')
+                .attr('class', 'ms')
+                .attr('transform', function (d, index) {
+                    var x = Math.floor(index / mileStonesPerColumn) * 5;
+                    var y = (index % mileStonesPerColumn) * _this.dayHeight;
+                    d.xdif = x;
+                    d.ydif = y;
+                    d.x = 0;
+                    d.y = 0;
+                    return 'translate(' + x + ',' + y + ')';
+                })
+
+
+
+            mileStones.append('path').attr("d", d3.svg.symbol().type('diamond').size(600))
+                .attr("fill", function (d) {
+                    return colorFun(d.type);
+                })
+                .attr('opacity', 0.5)
+
+
+            mileStones.append('text')
+                .text(function(d){
+                    return d.name
+                })
+                .attr('dy', 30)
+                .style("text-anchor", "middle")
+
+            _this.workStreams = workStreams;
+
+
+            var dragMove = function(d){
+                d.x = Math.floor(d3.event.x/_this.dayWidth) * _this.dayWidth;
+                var x = d.xdif + d.x;
+                var y = d.ydif;
+                d3.select(this).attr('transform', 'translate('+x+','+y+')' )
+            }
+
+            var dragEnd = function(d){
+                var diffDays = (d.x/_this.dayWidth);
+                d.date += (diffDays * msPerDay);
+                _this.renderWorkStream();
+            }
+
+
+            var mileStoneDrag = d3.behavior.drag()
+                .origin(Object)
+                .on('drag', dragMove)
+                .on('dragend', dragEnd)
+
+            mileStones.call(mileStoneDrag)
+        },
+        updateWorkStream: function () {
+            var _this = this;
+            var startDate = new Date(this.startYear, this.startMonth, this.startDay).getTime();
+            if(!_this.workStreams){
+                return;
+            }
+            _this.workStreams.selectAll('g.dt')
+                .attr('transform', function (d) {
+                    var date = d.date;
+                    var dayDiff = (date - startDate) / msPerDay;
+                    var x = dayDiff * _this.dayWidth;
+                    var y = 0;
+                    return 'translate(' + x + ',' + y + ')';
+                })
+
         }
+    }
+
+    var getUniqueId = (function () {
+        var counter = 10;
+        return function () {
+            return counter++;
+        }
+    })()
+
+    var generateWorkStreamData = function () {
+        var data = [];
+        var mileStoneTypes = ['E', 'P', 'W']
+        for (var i = 0; i < 4; i++) {
+            var wsName = 'ws_' + i;
+            data.push({
+                name:wsName,
+                id: i,
+                milestones: _.map(_.range(0, 6), function (index) {
+                    return {
+                        id: getUniqueId(),
+                        name: 'ms_' + getUniqueId(),
+                        date: new Date(2013, 0, Math.ceil(Math.random() * 5)).getTime(),
+                        type: mileStoneTypes[Math.floor(Math.random() * 3)]
+                    }
+                })
+            })
+        }
+        console.log(data);
+        return data;
     }
 
 
@@ -375,8 +616,11 @@
 
     var lbpController = new LBPController({
         rootEl: d3.select('.lbp-container'),
-        dayWidth: 50
+        dayWidth: 50,
+        scale:'day'
     })
+
+    lbpController.setWorkStreamData(generateWorkStreamData());
 
     lbpController.renderLBP();
 
